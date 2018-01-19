95cd5616d9acaa422b374b46587e322ab96e9186
Chris Akroyd <cra5@kent.ac.uk>
2018-01-19 23:49:53 +0000

    CUDNN based model for faster training over the large dataset.

--- src/models/bi_cudnn_rnn_attention.py
+++ src/models/bi_cudnn_rnn_attention.py
@@ -1,6 +1,6 @@
-from keras.layers import Dense, Embedding, Bidirectional, Dropout, GRU, BatchNormalization, SpatialDropout1D
+from keras.layers import Dense, Embedding, Bidirectional, Dropout, BatchNormalization, SpatialDropout1D, CuDNNGRU
 from keras.models import Sequential
-from keras.optimizers import RMSprop
+from keras.optimizers import Adam, RMSprop
 
 from .layers.Attention import HierarchicalAttention as Attention
 from .metrics import f1
@@ -13,13 +13,13 @@ CLIP_NORM = 1.0
 NUM_CLASSES = 12
 
 
-class BiLSTMAttention:
+class CUDNNBiRNNAttention:
     def __init__(self, num_classes=5):
         self.BATCH_SIZE = BATCH_SIZE
         self.EPOCHS = EPOCHS
         self.LEARN_RATE = LEARN_RATE
         self.num_classes = num_classes
-        self.checkpoint_path = './model_checkpoints/bi_lstm_attention.hdf5'
+        self.checkpoint_path = './model_checkpoints/CUDNN_RNN.hdf5'
 
     def create_model(self, vocab_size, embedding_matrix, input_length=5000, embed_dim=100):
         model = Sequential()
@@ -28,8 +28,8 @@ class BiLSTMAttention:
         model.add(SpatialDropout1D(0.2))
         model.add(BatchNormalization())
 
-        model.add(Bidirectional(GRU(150, return_sequences=True)))
-        model.add(Bidirectional(GRU(150, return_sequences=True)))
+        model.add(Bidirectional(CuDNNGRU(150, return_sequences=True)))
+        model.add(Bidirectional(CuDNNGRU(150, return_sequences=True)))
 
         model.add(Attention())
         model.add(Dropout(0.5))
@@ -40,4 +40,3 @@ class BiLSTMAttention:
         model.compile(loss='categorical_crossentropy', optimizer=RMSprop(lr=self.LEARN_RATE, clipnorm=CLIP_NORM), metrics=['accuracy', f1])
 
         return model
-
