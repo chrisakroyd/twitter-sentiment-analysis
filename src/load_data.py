30ef0f9987180ceafa1c36169cbdeaca6a6d0280
Christopher Akroyd <chris@chrisakroyd.com>
2018-01-22 23:48:18 +0000

    Better pre-processing, positive/negative classifier.

--- src/load_data.py
+++ src/load_data.py
@@ -14,23 +14,34 @@ from .sem_eval_utilities import concat_load_tsvs
 
 RANDOM_SEED = 59185
 CONTROL_BALANCE = True
-DATASET_SIZE = 50000
+DATASET_SIZE = 500000
 
 mentions_regex = re.compile('(?<=^|(?<=[^a-zA-Z0-9-_.]))@([A-Za-z_]+[A-Za-z0-9_]+)')
 hash_tag_regex = re.compile('(?<=^|(?<=[^a-zA-Z0-9-_.]))#([A-Za-z]+[A-Za-z0-9]+)')
 url_regex = re.compile('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+')
+# Smile, Laugh, Love, Wink emoticon regex : :), : ), :-), (:, ( :, (-:, :'), :D, : D, :-D, xD, x-D, XD, X-D,
+# <3, ;-), ;), ;-D, ;D, (;, (-;
+emo_pos_regex = re.compile('(:\s?\)|:-\)|\(\s?:|\(-:|:\'\))|(:\s?D|:-D|x-?D|X-?D)|(<3)|(;-?\)|;-?D|\(-?;)')
+# Sad & Cry emoticon regex: :-(, : (, :(, ):, )-:, :,(, :'(, :"(
+emo_neg_regex = re.compile('(:\s?\(|:-\(|\)\s?:|\)-:)|(:,\(|:\'\(|:"\()')
+
+collapse_letters_regex = re.compile('(.)\1+')
 
 
 def get_data_sent_140(path):
     df = pd.read_csv(path, names=['class', 'id', 'date', 'query', 'user', 'text'], encoding='latin-1')
+    df['class'] = df['class'].replace({0: 'negative', 4: 'positive'})
+    print(df['class'].value_counts())
     df = shuffle(df)
-    return df[:DATASET_SIZE]
+    # return df[:DATASET_SIZE]
+    return df
 
 
 def get_data_sem_eval(data_dir):
     df = concat_load_tsvs(data_dir)
     print(df['class'].value_counts())
     return df
+
 
 def pre_process(df):
     new_df = pd.DataFrame.from_items([(name, pd.Series(data=None, dtype=series.dtype)) for name, series in df.iteritems()])
@@ -43,6 +54,9 @@ def pre_process(df):
             tweet['text'] = mentions_regex.sub('<MENTION>', tweet['text'])
             tweet['text'] = hash_tag_regex.sub('<HASH_TAG>', tweet['text'])
             tweet['text'] = url_regex.sub('<URL>', tweet['text'])
+            tweet['text'] = emo_pos_regex.sub('<EMO_POS>', tweet['text'])
+            tweet['text'] = emo_neg_regex.sub('<EMO_NEG>', tweet['text'])
+            tweet['text'] = collapse_letters_regex.sub(r'\1\1', tweet['text'])
             rows.append(tweet)
         except:
             pass
@@ -57,8 +71,6 @@ def load_data(path, data_set='sem_eval', max_features=5000,):
         df = get_data_sem_eval(path)
     elif data_set == 'sent_140':
         df = get_data_sent_140(path)
-    elif data_set == 'custom':
-        df = get_custom_data(path)
     else:
         print('INVALID DATA GENERATOR SPECIFIED')
 
@@ -73,8 +85,12 @@ def load_data(path, data_set='sem_eval', max_features=5000,):
     X = pad_sequences(tokenizer.texts_to_sequences(data_set['text']))
     y = label_binarizer.fit_transform(data_set['class'])
 
+    if len(label_binarizer.classes_) == 2:
+        y = np.hstack((y, 1 - y))
+
     print('Number of Data Samples:' + str(len(X)))
 
     x_train, x_val, y_train, y_val = train_test_split(X, y, test_size=0.2, stratify=y)
+    num_classes = len(label_binarizer.classes_)
 
-    return (np.array(x_train), np.array(y_train)), (np.array(x_val), np.array(y_val)), word_index
+    return (np.array(x_train), np.array(y_train)), (np.array(x_val), np.array(y_val)), word_index, num_classes
--- src/models/bi_cudnn_rnn_attention.py
+++ src/models/bi_cudnn_rnn_attention.py
@@ -1,12 +1,13 @@
-from keras.layers import Dense, Embedding, Bidirectional, Dropout, BatchNormalization, SpatialDropout1D, CuDNNGRU
+from keras.layers import Dense, Embedding, Bidirectional, Dropout, BatchNormalization, SpatialDropout1D, CuDNNLSTM, GaussianNoise
 from keras.models import Sequential
-from keras.optimizers import Adam, RMSprop
+from keras.regularizers import l2
+from keras.optimizers import RMSprop
 
-from .layers.Attention import HierarchicalAttention as Attention
+from .layers.Attention import FeedForwardAttention as Attention
 from .metrics import f1
 
 # HPARAMs
-BATCH_SIZE = 64
+BATCH_SIZE = 128
 EPOCHS = 50
 LEARN_RATE = 0.001
 CLIP_NORM = 1.0
@@ -19,20 +20,24 @@ class CUDNNBiRNNAttention:
         self.EPOCHS = EPOCHS
         self.LEARN_RATE = LEARN_RATE
         self.num_classes = num_classes
-        self.checkpoint_path = './model_checkpoints/CUDNN_RNN.hdf5'
+        self.checkpoint_path = './model_checkpoints/Pos_Neg_Classifier.hdf5'
 
-    def create_model(self, vocab_size, embedding_matrix, input_length=5000, embed_dim=100):
+    def create_model(self, vocab_size, embedding_matrix, input_length=5000, embed_dim=200):
         model = Sequential()
 
         model.add(Embedding(vocab_size, embed_dim, weights=[embedding_matrix], input_length=input_length))
-        model.add(SpatialDropout1D(0.2))
+        model.add(SpatialDropout1D(0.3))
+        model.add(GaussianNoise(0.2))
         model.add(BatchNormalization())
 
-        model.add(Bidirectional(CuDNNGRU(150, return_sequences=True)))
-        model.add(Bidirectional(CuDNNGRU(150, return_sequences=True)))
+        model.add(Bidirectional(CuDNNLSTM(100, return_sequences=True, recurrent_regularizer=l2(0.00001))))
+        model.add(Dropout(0.5))
+        model.add(Bidirectional(CuDNNLSTM(100, return_sequences=True, recurrent_regularizer=l2(0.00001))))
+        model.add(Dropout(0.5))
 
         model.add(Attention())
         model.add(Dropout(0.5))
+
         model.add(Dense(self.num_classes, activation='softmax'))
 
         model.summary()
