5e03390a9c391ece7f04af2bf4c851a2c0c8f023
Christopher Akroyd <chris@chrisakroyd.com>
2018-03-23 22:44:10 +0000

    top k instead of max pool, affinity vector model.
    Former-commit-id: 2d832fad1120d89a9d227f556835a1034cf6cc6e

--- src/models/bi_lstm_affin.py
+++ src/models/bi_lstm_affin.py
@@ -6,21 +6,11 @@ from src.metrics import f1
 
 from keras.backend import squeeze, sum
 
-import tensorflow as tf
-
 # HPARAMs
 BATCH_SIZE = 128
 EPOCHS = 50
 LEARN_RATE = 0.001
 NUM_CLASSES = 12
-
-top_k = 10
-
-
-def _top_k(x):
-    x = tf.transpose(x, [0, 2, 1])
-    k_max = tf.nn.top_k(x, k=top_k)
-    return tf.reshape(k_max[0], (-1, 2 * 120 * top_k))
 
 
 class BiLSTMConcPool:
@@ -30,10 +20,11 @@ class BiLSTMConcPool:
         self.LEARN_RATE = LEARN_RATE
         self.num_classes = num_classes
 
-    def create_model(self, vocab_size, embedding_matrix, input_length=5000, embed_dim=200):
+    def create_model(self, vocab_size, embedding_matrix, afinn_matrix, input_length=5000, embed_dim=200):
         input = Input(shape=(input_length, ))
 
         embedding = Embedding(vocab_size, embed_dim, weights=[embedding_matrix], input_length=input_length)(input)
+        affin_embedding = Embedding(vocab_size, 1, weights=[afinn_matrix], input_length=input_length)(input)
 
         spatial_dropout_1 = SpatialDropout1D(0.2)(embedding)
 
@@ -49,10 +40,12 @@ class BiLSTMConcPool:
 
         last_state = concatenate([forward_h, backward_h], name='last_state')
         avg_pool = GlobalAveragePooling1D()(bi_gru_1)
-        k_max = Lambda(_top_k)(bi_gru_1)
+        max_pool = GlobalMaxPooling1D()(bi_gru_1)
 
-        conc = concatenate([last_state, k_max, avg_pool], name='conc_pool')
+        # affin_vec = Lambda(lambda x: sum(squeeze(x, axis=-1), axis=-1))(affin_embedding)
+        affin_vec = Lambda(lambda x: squeeze(x, axis=-1))(affin_embedding)
 
+        conc = concatenate([last_state, max_pool, avg_pool, affin_vec], name='conc_pool')
         drop_1 = Dropout(0.5)(conc)
 
         outputs = Dense(self.num_classes, activation='softmax')(drop_1)
@@ -62,7 +55,7 @@ class BiLSTMConcPool:
         return model
 
     def build(self, vocab_size, embedding_matrix, afinn_matrix, input_length=5000, embed_dim=200, summary=True):
-        model = self.create_model(vocab_size, embedding_matrix, input_length, embed_dim)
+        model = self.create_model(vocab_size, embedding_matrix, afinn_matrix, input_length, embed_dim)
 
         model.compile(loss='categorical_crossentropy',
                       optimizer=Nadam(lr=self.LEARN_RATE),
