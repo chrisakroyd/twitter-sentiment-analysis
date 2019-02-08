Rewrote metrics (recall, precision, f1) with tensorflow ops for performance, fixed error where masking was applied post-softmax.

src/layers/attention.py
@@ -32,11 +32,11 @@ class Attention(tf.keras.layers.Layer):
         eij = tf.squeeze(tf.keras.backend.dot(x, self.kernel), axis=-1)
         eij = tf.tanh(eij)
 
-        a = tf.exp(eij)
-
         # apply mask.
         if mask is not None:
-            a *= tf.cast(mask, tf.float32)
+            eij *= tf.cast(mask, tf.float32)
+
+        a = tf.exp(eij)
 
         a /= tf.cast(tf.reduce_sum(a, axis=1, keepdims=True) + tf.keras.backend.epsilon(), tf.float32)
         att_weights = a

src/metrics.py
@@ -1,34 +1,68 @@
 import random
 import tensorflow as tf
-import numpy as np
-import sklearn.metrics as metrics
 
 
 def evaluate_list(preds, data_type=None, writer=None, global_step=0, subsample_ratio=None):
-    recall, precision, f1 = 0., 0., 0.
+    avg_recall, avg_precision, avg_f1 = 0., 0., 0.
 
     if subsample_ratio is not None:
         preds = [preds[i] for i in random.sample(range(len(preds)), int(len(preds) * subsample_ratio))]
 
-    for _, pred, labels in preds:
-        pred = np.argmax(pred.tolist(), axis=1)
-        labels = labels.tolist()
-        recall += metrics.recall_score(labels, pred, average='micro')
-        precision += metrics.precision_score(labels, pred, average='micro')
+    # for _, pred, labels in preds:
+    for rec, prec, f_score in preds:
+        avg_recall += rec
+        avg_precision += prec
+        avg_f1 += f_score
 
-    recall /= len(preds)
-    precision /= len(preds)
-    f1 = 2 * (precision * recall) / (precision + recall)
+    avg_recall /= len(preds)
+    avg_precision /= len(preds)
+    avg_f1 /= len(preds)
 
     results = {
-        'recall': recall,
-        'precision': precision,
-        'f1': f1,
+        'recall': avg_recall,
+        'precision': avg_precision,
+        'f1': avg_f1,
     }
 
     add_metric_summaries(results, data_type, writer, global_step)
 
     return results
+
+
+def recall(y_true, y_pred, epsilon=1e-07):
+    """ Recall metric. Computes a batch-wise average of recall for multi-label classification. """
+    y_true = tf.cast(y_true, dtype=tf.float32)
+    y_pred = tf.cast(y_pred, dtype=tf.float32)
+
+    true_positives = tf.reduce_sum(tf.round(tf.clip_by_value(y_true * y_pred, 0, 1)))
+    possible_positives = tf.reduce_sum(tf.round(tf.clip_by_value(y_true, 0, 1)))
+
+    rec = true_positives / (possible_positives + tf.constant(epsilon, dtype=tf.float32))
+    return rec
+
+
+def precision(y_true, y_pred, epsilon=1e-07):
+    """ Precision metric. Computes a batch-wise average of precision for multi-label classification. """
+    y_true = tf.cast(y_true, dtype=tf.float32)
+    y_pred = tf.cast(y_pred, dtype=tf.float32)
+
+    true_positives = tf.reduce_sum(tf.round(tf.clip_by_value(y_true * y_pred, 0, 1)))
+    predicted_positives = tf.reduce_sum(tf.round(tf.clip_by_value(y_pred, 0, 1)))
+
+    prec = true_positives / (predicted_positives + tf.constant(epsilon, dtype=tf.float32))
+    return prec
+
+
+def harmonic_mean(x_1, x_2):
+    """ Calculates the harmonic mean of two values """
+    return 2 * ((x_1 * x_2) / (x_1 + x_2))
+
+
+def f1(y_true, y_pred):
+    """ Calculates the batch-wise F1 score for multi-label classification. """
+    prec = precision(y_true, y_pred)
+    rec = recall(y_true, y_pred)
+    return harmonic_mean(prec, rec)
 
 
 def add_metric_summaries(metrics, data_type=None, writer=None, global_step=0):

src/models/lstm_attention.py
@@ -50,7 +50,7 @@ class LSTMAttention(tf.keras.models.Model):
         return logits, preds, attn_weights
 
     def compute_loss(self, logits, labels, l2=None):
-        loss = tf.nn.sparse_softmax_cross_entropy_with_logits(logits=logits, labels=labels)
+        loss = tf.nn.softmax_cross_entropy_with_logits_v2(logits=logits, labels=labels)
         loss = tf.reduce_mean(loss)
 
         if l2 is not None and l2 > 0.0:

src/pipeline.py
@@ -60,21 +60,33 @@ def index_lookup(data, tables, char_limit=16, num_parallel_calls=4, has_labels=T
         chars = tf.string_split(fields['tokens'], delimiter='')
         chars = tf.sparse.to_dense(char_table.lookup(chars), default_value=-1) + 1
         chars = chars[:, :char_limit]
+        fields['words'] = tokens
+        fields['chars'] = chars
+        return fields
 
+    data = data.map(_lookup, num_parallel_calls=num_parallel_calls)
+    return data
+
+
+def format_output(data, num_classes, num_parallel_calls=4):
+    """ Casts tensors to their intended dtypes and converts label tensor to be one-hot. """
+
+    def _lookup(fields):
         out_dict = {
-            'words': tf.cast(tokens, dtype=tf.int32),
-            'chars': tf.cast(chars, dtype=tf.int32),
+            'words': tf.cast(fields['words'], dtype=tf.int32),
+            'chars': tf.cast(fields['chars'], dtype=tf.int32),
             'num_tokens': tf.cast(fields['num_tokens'], dtype=tf.int32),
         }
 
-        if has_labels:
+        if 'label' in fields:
             out_dict.update({
-                'label': tf.cast(fields['label'], dtype=tf.int32),
+                'label': tf.one_hot(fields['label'], num_classes, dtype=tf.int32)
             })
 
         return out_dict
 
     data = data.map(_lookup, num_parallel_calls=num_parallel_calls)
+
     return data
 
 
@@ -92,7 +104,7 @@ def create_buckets(bucket_size, max_size, bucket_ranges=None):
     return bucket_ranges
 
 
-def get_padded_shapes(max_tokens=-1, max_characters=16, has_labels=True):
+def get_padded_shapes(max_tokens=-1, max_characters=16, num_classes=2, has_labels=True):
     """ Creates a dict of key: shape mappings for padding batches.
 
         @TODO This is a pretty ugly solution to support labelled/unlabelled modes, refactor target?
@@ -100,6 +112,7 @@ def get_padded_shapes(max_tokens=-1, max_characters=16, has_labels=True):
         Args:
             max_tokens: Max size of the context, -1 to pad to max within the batch.
             max_characters: Max number of characters, -1 to pad to max within the batch.
+            num_classes: Number of classes in the dataset.
             has_labels: Include padded shape for answer_starts and answer_ends.
         Returns:
             A dict mapping of key: shape
@@ -112,8 +125,9 @@ def get_padded_shapes(max_tokens=-1, max_characters=16, has_labels=True):
 
     if has_labels:
         shape_dict.update({
-            'label': [],
+            'label': [num_classes],
         })
+
     return shape_dict
 
 
@@ -133,7 +147,7 @@ def create_lookup_tables(vocabs):
     return tables
 
 
-def create_pipeline(params, tables, record_paths, training=True):
+def create_pipeline(params, tables, record_paths, num_classes, training=True):
     """ Function that creates an input pipeline for train/eval.
 
         Optionally uses bucketing to generate batches of a similar length. Output tensors
@@ -147,18 +161,21 @@ def create_pipeline(params, tables, record_paths, training=True):
         Returns:
             A `tf.data.Dataset` object and an initializable iterator.
     """
-    parallel_calls = num_parallel_calls(params)
+    parallel_calls = get_num_parallel_calls(params)
 
     data = tf_record_pipeline(record_paths, params.tf_record_buffer_size, parallel_calls)
     data = data.cache()
+
     if training:
         data = data.apply(tf.data.experimental.shuffle_and_repeat(buffer_size=params.shuffle_buffer_size))
     else:
         data = data.repeat()
+
     # Perform word -> index mapping.
     data = index_lookup(data, tables, char_limit=params.char_limit,
                         num_parallel_calls=parallel_calls)
-    padded_shapes = get_padded_shapes(max_tokens=params.max_tokens, max_characters=params.char_limit)
+    data = format_output(data, num_classes, num_parallel_calls=parallel_calls)
+    padded_shapes = get_padded_shapes(max_tokens=params.max_tokens, max_characters=params.char_limit, num_classes=num_classes)
 
     if params.bucket and training:
         buckets = create_buckets(params.bucket_size, params.max_tokens, params.bucket_ranges)
@@ -196,7 +213,7 @@ def create_demo_pipeline(params, tables, data):
         Returns:
             A `tf.data.Dataset` object and an initializable iterator.
     """
-    parallel_calls = num_parallel_calls(params)
+    parallel_calls = get_num_parallel_calls(params)
 
     data = tf.data.Dataset.from_tensor_slices(dict(data))
     data = index_lookup(data, tables, char_limit=params.char_limit,
@@ -219,8 +236,8 @@ def create_placeholders():
     }
 
 
-def num_parallel_calls(params):
-    # If we aren't given a parallel calls parameter, default to the systems CPU count.
+def get_num_parallel_calls(params):
+    """ Calculates the number of parallel calls we can make, if no number given returns the CPU count. """
     parallel_calls = params.parallel_calls
     if parallel_calls < 0:
         parallel_calls = os.cpu_count()

test.py
@@ -13,23 +13,29 @@ def test(sess_config, params):
     vocabs = util.load_vocab_files(paths=(word_index_path, char_index_path))
     word_matrix, trainable_matrix, character_matrix = util.load_numpy_files(paths=embedding_paths)
     meta = util.load_json(meta_path)
+    num_classes = meta['num_classes']
 
     with tf.device('/cpu:0'):
         tables = pipeline.create_lookup_tables(vocabs)
         val_tfrecords = util.tf_record_paths(params, training=False)
-        val_set, iterator = pipeline.create_pipeline(params, tables, val_tfrecords, training=False)
+        val_set, iterator = pipeline.create_pipeline(params, tables, val_tfrecords, num_classes, training=False)
 
     with tf.Session(config=sess_config) as sess:
         sess.run(iterator.initializer)
         sess.run(tf.tables_initializer())
 
-        model = models.LSTMAttention(word_matrix, character_matrix, trainable_matrix, meta['num_classes'], params)
+        model = models.LSTMAttention(word_matrix, character_matrix, trainable_matrix, num_classes, params)
 
         placeholders = iterator.get_next()
         # Features and labels.
         model_inputs = train_utils.inputs_as_tuple(placeholders)
         label_tensor = train_utils.labels_as_tuple(placeholders)[0]
         logits, prediction, _ = model(model_inputs, training=False)
+
+        recall = metrics.recall(label_tensor, prediction)
+        precision = metrics.precision(label_tensor, prediction)
+        f1 = metrics.harmonic_mean(precision, recall)
+        test_outputs = [recall, precision, f1]
 
         sess.run(tf.global_variables_initializer())
 
@@ -39,12 +45,12 @@ def test(sess_config, params):
         preds = []
         # +1 for uneven batch values, +1 for the range.
         for _ in tqdm(range(1, (meta['num_val'] // params.batch_size + 1) + 1)):
-            pred, label = sess.run([prediction, label_tensor],
-                                   feed_dict={
-                                       model.dropout: 0.0,
-                                       model.attn_dropout: 0.0,
-                                   })
-            preds.append((0.0, pred, label,))
+            recall, precision, f1 = sess.run(test_outputs,
+                                             feed_dict={
+                                                 model.dropout: 0.0,
+                                                 model.attn_dropout: 0.0,
+                                             })
+            preds.append((recall, precision, f1,))
 
         # Evaluate the predictions and reset the train result list for next eval period.
         val_metrics = metrics.evaluate_list(preds)

train.py
@@ -14,21 +14,22 @@ def train(sess_config, params):
     vocabs = util.load_vocab_files(paths=(word_index_path, char_index_path))
     word_matrix, trainable_matrix, character_matrix = util.load_numpy_files(paths=embedding_paths)
     meta = util.load_json(meta_path)
+    num_classes = meta['num_classes']
 
     with tf.device('/cpu:0'):
         tables = pipeline.create_lookup_tables(vocabs)
 
         train_tfrecords = util.tf_record_paths(params, training=True)
         val_tfrecords = util.tf_record_paths(params, training=False)
-        train_set, train_iter = pipeline.create_pipeline(params, tables, train_tfrecords, training=True)
-        _, val_iter = pipeline.create_pipeline(params, tables, val_tfrecords, training=False)
+        train_set, train_iter = pipeline.create_pipeline(params, tables, train_tfrecords, num_classes, training=True)
+        _, val_iter = pipeline.create_pipeline(params, tables, val_tfrecords, num_classes, training=False)
 
     with tf.Session(config=sess_config) as sess:
         sess.run([tf.tables_initializer(), train_iter.initializer, val_iter.initializer])
         handle = tf.placeholder(tf.string, shape=[])
         iterator = tf.data.Iterator.from_string_handle(handle, train_set.output_types, train_set.output_shapes)
 
-        model = models.LSTMAttention(word_matrix, character_matrix, trainable_matrix, meta['num_classes'], params)
+        model = models.LSTMAttention(word_matrix, character_matrix, trainable_matrix, num_classes, params)
 
         placeholders = iterator.get_next()
         # Features and labels.
@@ -48,8 +49,12 @@ def train(sess_config, params):
                                                   beta2=params.beta2,
                                                   epsilon=params.epsilon)
 
-        train_outputs = [loss_op, prediction, label_tensor, train_op]
-        val_outputs = [loss_op, prediction, label_tensor]
+        recall = metrics.recall(label_tensor, prediction)
+        precision = metrics.precision(label_tensor, prediction)
+        f1 = metrics.harmonic_mean(precision, recall)
+
+        train_outputs = [recall, precision, f1, train_op]
+        val_outputs = [recall, precision, f1]
         sess.run(tf.global_variables_initializer())
         # Saver boilerplate
         writer = tf.summary.FileWriter(log_dir, graph=sess.graph)
@@ -71,20 +76,21 @@ def train(sess_config, params):
 
         for _ in range(int(total_steps - global_step)):
             global_step = sess.run(model.global_step) + 1
-            loss, pred, label, _ = sess.run(fetches=train_outputs, feed_dict={handle: train_handle})
-            train_preds.append((loss, pred, label, ))
+            # loss, pred, label, _ = sess.run(fetches=train_outputs, feed_dict={handle: train_handle})
+            recall, precision, f1, _ = sess.run(fetches=train_outputs, feed_dict={handle: train_handle})
+            train_preds.append((recall, precision, f1, ))
             pbar.update()
             # Save at the end of each epoch
             if (global_step % (meta['num_train'] // params.batch_size)) == 0 or global_step == total_steps:
                 val_preds = []
                 for _ in range(meta['num_val']):
-                    loss, pred, label = sess.run(fetches=val_outputs,
-                                                 feed_dict={
-                                                    handle: val_handle,
-                                                    model.dropout: 0.0,
-                                                    model.attn_dropout: 0.0,
-                                                  })
-                    val_preds.append((loss, pred, label, ))
+                    recall, precision, f1 = sess.run(fetches=val_outputs,
+                                                     feed_dict={
+                                                        handle: val_handle,
+                                                        model.dropout: 0.0,
+                                                        model.attn_dropout: 0.0,
+                                                      })
+                    val_preds.append((recall, precision, f1, ))
 
                 metrics.evaluate_list(train_preds, 'train', writer, global_step)
                 val_metrics = metrics.evaluate_list(val_preds, 'val', writer, global_step)