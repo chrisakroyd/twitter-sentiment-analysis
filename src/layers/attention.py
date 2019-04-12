L2 loss over non-bias variables, fixed bug with pipeline always returning padded shapes, fixed + implemented attention masking.

src/layers/attention.py
@@ -1,24 +1,19 @@
 import tensorflow as tf
+from src import layers
 
 
 class Attention(tf.keras.layers.Layer):
-    def __init__(self,
-                 kernel_initializer='glorot_uniform',
-                 kernel_regularizer=None,
-                 return_attention=False,
+    def __init__(self, return_attention=False, epsilon=1e-07,
                  **kwargs):
         super(Attention, self).__init__(**kwargs)
         self.supports_masking = True
-        self.kernel_initializer = tf.keras.initializers.get(kernel_initializer)
-        self.kernel_regularizer = tf.keras.regularizers.get(kernel_regularizer)
         self.return_attention = return_attention
+        self.epsilon = epsilon
 
     def build(self, input_shape):
         assert len(input_shape) == 3
         self.kernel = self.add_weight(shape=(int(input_shape[-1]), 1, ),
-                                      initializer=self.kernel_initializer,
                                       name='{}_W'.format(self.name),
-                                      regularizer=self.kernel_regularizer,
                                       trainable=True)
         self.built = True
 
@@ -32,13 +27,12 @@ class Attention(tf.keras.layers.Layer):
         eij = tf.squeeze(tf.keras.backend.dot(x, self.kernel), axis=-1)
         eij = tf.tanh(eij)
 
-        # apply mask.
         if mask is not None:
-            eij *= tf.cast(mask, tf.float32)
+            eij = layers.apply_mask(eij, mask)
 
         a = tf.exp(eij)
 
-        a /= tf.cast(tf.reduce_sum(a, axis=1, keepdims=True) + tf.keras.backend.epsilon(), tf.float32)
+        a /= tf.cast(tf.reduce_sum(a, axis=1, keepdims=True) + tf.keras.backend.epsilon(), dtype=tf.float32)
         att_weights = a
 
         weighted_input = x * tf.expand_dims(a, axis=-1)

src/models/lstm_attention.py
@@ -32,7 +32,7 @@ class LSTMAttention(tf.keras.models.Model):
 
     def call(self, x, training=None, mask=None):
         words, chars, tags, num_tokens = x
-
+        attn_mask = layers.create_mask(num_tokens, maxlen=tf.reduce_max(num_tokens))
         text_emb = self.embedding([words, chars], training=training)
 
         text_emb = tf.concat([text_emb, tags], axis=-1)
@@ -43,7 +43,7 @@ class LSTMAttention(tf.keras.models.Model):
         rnn_2_out = self.rnn_2(rnn_1_out)
         rnn_2_out = self.drop_2(rnn_2_out, training=training)
 
-        attn_out, attn_weights = self.attention(rnn_2_out)
+        attn_out, attn_weights = self.attention(rnn_2_out, mask=attn_mask)
         attn_out = self.drop_3(attn_out, training=training)
 
         logits = self.out(attn_out)
@@ -56,7 +56,9 @@ class LSTMAttention(tf.keras.models.Model):
         loss = tf.reduce_mean(loss)
 
         if l2 is not None and l2 > 0.0:
-            l2_loss = train_utils.l2_ops(l2)
+            variables = tf.trainable_variables()
+            variables = [v for v in variables if 'bias' not in v.name and 'scale' not in v.name]
+            l2_loss = train_utils.l2_ops(l2, variables=variables)
             loss = loss + l2_loss
 
         return loss

src/pipeline.py
@@ -39,14 +39,11 @@ def index_lookup(data, tables, char_limit=16, num_parallel_calls=4):
         To save memory + hard drive space we store contexts and queries as tokenised strings. Therefore we need
         to perform two tasks; Extract characters and map words + chars to an index for the embedding layer.
 
-        @TODO This is a pretty ugly solution to support labelled/unlabelled modes, refactor target?
-
         Args:
             data: A `tf.data.Dataset` object.
             tables: A tuple of contrib.lookup tables mapping string words to indices and string characters to indices.
             char_limit: Max number of characters per word.
             num_parallel_calls: An int for how many parallel lookups we perform.
-            has_labels: Include labels in the output dict.
         Returns:
             A `tf.data.Dataset` object.
     """
@@ -74,7 +71,7 @@ def index_lookup(data, tables, char_limit=16, num_parallel_calls=4):
     return data
 
 
-def format_output(data, num_classes, num_tags, num_parallel_calls=4):
+def post_processing(data, num_classes, num_tags, num_parallel_calls=4):
     """ Casts tensors to their intended dtypes and converts label tensor to be one-hot. """
 
     def _lookup(fields):
@@ -113,8 +110,6 @@ def create_buckets(bucket_size, max_size, bucket_ranges=None):
 
 def get_padded_shapes(max_tokens=-1, max_characters=16, num_classes=2, num_tags=50, has_labels=True):
     """ Creates a dict of key: shape mappings for padding batches.
-
-        @TODO This is a pretty ugly solution to support labelled/unlabelled modes, refactor target?
 
         Args:
             max_tokens: Max size of the context, -1 to pad to max within the batch.
@@ -185,9 +180,7 @@ def create_pipeline(params, tables, record_paths, num_classes, num_tags, trainin
     # Perform word -> index mapping.
     data = index_lookup(data, tables, char_limit=params.char_limit,
                         num_parallel_calls=parallel_calls)
-    data = format_output(data, num_classes, num_tags, num_parallel_calls=parallel_calls)
-    padded_shapes = get_padded_shapes(max_tokens=params.max_tokens, max_characters=params.char_limit,
-                                      num_classes=num_classes, num_tags=num_tags)
+    data = post_processing(data, num_classes, num_tags, num_parallel_calls=parallel_calls)
 
     if params.bucket and training:
         buckets = create_buckets(params.bucket_size, params.max_tokens, params.bucket_ranges)
@@ -197,10 +190,11 @@ def create_pipeline(params, tables, record_paths, num_classes, num_tags, trainin
 
         data = data.apply(
             tf.data.experimental.bucket_by_sequence_length(element_length_func=length_fn,
-                                                           padded_shapes=padded_shapes,
                                                            bucket_batch_sizes=[params.batch_size] * (len(buckets) + 1),
                                                            bucket_boundaries=buckets))
     else:
+        padded_shapes = get_padded_shapes(max_characters=params.char_limit,
+                                          num_classes=num_classes, num_tags=num_tags)
         data = data.padded_batch(
             batch_size=params.batch_size,
             padded_shapes=padded_shapes,