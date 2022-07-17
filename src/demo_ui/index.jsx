bd1b1fa18b6e09efb031632f037bdc40fdc61c50
Christopher Akroyd <chris@chrisakroyd.com>
2022-07-17 18:31:18 +0100

    Fixed the project so that it could build.

--- src/demo_ui/index.jsx
+++ src/demo_ui/index.jsx
@@ -35,16 +35,16 @@ const reducers = combineReducers({
 
 const defaultState = {
   predictions: {
-    tokens: ['Hey', '<user>', 'I', 'like', 'you', ',', 'check', 'this', 'out', '<url>'],
-    enabled: new Array(10).fill(true),
+    tokens: ['Hey', ',', 'I', 'love', 'this', 'website', ',', 'check', 'it', 'out', '<url>'],
+    enabled: new Array(11).fill(true),
     attentionWeights: [0.046337228268384933, 0.05221894010901451, 0.0850118100643158, 0.09090767055749893, 0.07975268363952637, 0.05724440887570381, 0.03628542646765709, 0.047189291566610336, 0.06301657110452652, 0.03931921720504761, 0.021808134391903877],
     label: 'Positive',
-    probs: [0.89, 0.06, 0.05],
+    probs: [0.49, 0.06, 0.45],
     loading: false,
     error: null,
   },
   text: {
-    text: 'Hey @_ChrisAkroyd_, I like you, check this out http://bbc.co.uk',
+    text: 'Hey, I love this website, check it out http://chrisakroyd.com',
     classes: { 0: 'neutral', 1: 'negative', 2: 'positive' },
     loading: false,
     error: null,
--- webpack.dev.js
+++ webpack.dev.js
@@ -1,12 +1,12 @@
 const common = require('./webpack.common.js');
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
-const merge = require('webpack-merge');
+const { merge } = require('webpack-merge');
 
 module.exports = merge(common, {
   mode: 'development',
-  devtool: 'cheap-module-eval-source-map',
+  devtool: 'eval-cheap-module-source-map',
   devServer: {
-    contentBase: './dist',
+    // contentBase: './dist',
   },
   plugins: [new BundleAnalyzerPlugin({
     analyzerMode: 'server',
